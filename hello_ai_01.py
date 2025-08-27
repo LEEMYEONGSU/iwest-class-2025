4a85e549efae44966cd31afec63bf0297f606cee
LMS <lms135246@gmail.com>
2025-08-27 17:49:13 +0900

    update model to use gpt-4o-mini in chat completion request

--- hello_ai_01.py
+++ hello_ai_01.py
@@ -7,16 +7,34 @@ load_dotenv()
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", default=None) or None
 print(repr(OPENAI_API_KEY))
 
-client = OpenAI(api_key=OPENAI_API_KEY)
-
-response = client.chat.completions.create(
-    model="gpt-4o",
-    messages = [
-        {   "role" : "system", "content": "당신은 서부발전의 장장장 대리입니다"},
-        {   "role" : "user", "content": "자기소개를 해주세요. 이름 3행시도 지어주세요"}
+client = OpenAI(api_key=OPENAI_API_KEY) 
+messages = [
+        {   "role" : "system", "content": "당신은 서부발전의 장장장 대리입니다"}
     ]
-)
 
-print("response.usage : ", response.usage)
-print(response.choices[0].message.content)
+# user_content = input("Human : ").strip()
+# messages.append(
+#      {   "role" : "user", "content": user_content}
+# )
+while True:
+    user_content = input("Human : ").strip()
+    if user_content in ["", "quit"]:
+        break
 
+    messages.append(
+        {"role" : "user", "content": user_content}
+    )
+
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=messages,
+    )
+
+    assistant_content: str = response.choices[0].message.content
+    messages.append(
+        {"role": "assistant", "content": assistant_content}
+    )
+    print("AI : ", assistant_content)
+
+    # print("response.usage : ", response.usage)
+    # print(response.choices[0].message.content)
---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+from tasks import create_email_body
+
+load_dotenv()  # 프로그램의 진입점에서 한번만 실행
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", default=None) or None
+
+create_email_body(
+    받는사람 = "이진석 대리",   #keyword parameter
+    용건 = "8월 업무보고",
+    핵심내용 = "우리 휴가 언제가나.",
+    api_key=OPENAI_API_KEY
+)
